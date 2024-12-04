df3879373da08cc4157c7295781ecaa54bfffaec
cooppoop <cmshu052@learner.dvusd.org>
2024-12-04 13:28:47 -0700

    did it again

--- wpfdex/wpfdex/MainWindow.xaml.vb
+++ wpfdex/wpfdex/MainWindow.xaml.vb
@@ -2,6 +2,7 @@
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         oconnor.Visibility = Visibility.Hidden
         liberty.Visibility = Visibility.Hidden
+        boulder_creek.Visibility = Visibility.Visible
         location1.Text = "anthem"
         pop.Text = "2577"
         mascot1.Text = "jaguar"
@@ -11,6 +12,7 @@
     Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
         boulder_creek.Visibility = Visibility.Hidden
         liberty.Visibility = Visibility.Hidden
+        oconnor.Visibility = Visibility.Visible
         location1.Text = "phoenix"
         pop.Text = "2604"
         mascot1.Text = "eagle"
@@ -20,6 +22,7 @@
     Private Sub liberty1_Click(sender As Object, e As RoutedEventArgs) Handles liberty1.Click
         boulder_creek.Visibility = Visibility.Hidden
         oconnor.Visibility = Visibility.Hidden
+        liberty.Visibility = Visibility.Visible
         location1.Text = "peoria"
         pop.Text = "2603"
         mascot1.Text = "lion"
